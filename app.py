cf381b198fb9b8ac067e72fdd7eda1b0c701872b
mccheung <mc_cheung@126.com>
2018-10-12 23:53:51 +0800

    make_response make response object

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import current_app
 
 app = Flask(__name__)
 
@@ -6,7 +8,16 @@ app = Flask(__name__)
 
 @app.route( '/' )
 def index():
-    return '<h1>Hello Flask</h1>'
+    user_agent = request.headers.get( 'User-Agent' )
+
+    response = make_response( '<h1>Hello Flask</h1>' )
+    response.set_cookie( 'anawer', '42' )
+    return response
+
+
+@app.route( '/user/<name>' )
+def user(name):
+    return 'User: {}!'.format( name )
 
 
 # app.add_url_rule( '/', 'index', index )
